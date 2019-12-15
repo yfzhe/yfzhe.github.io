a99fe2c39bbb3459a99b47eaee0b588b2d63cd49
yfzhe <yfangzhe@gmail.com>
2019-12-16 03:08:17 +0800

    update util.rkt

--- util.rkt
+++ util.rkt
@@ -1,5 +1,4 @@
 #lang racket/base
-
 (require (for-syntax racket/base syntax/parse)
          scribble/core
          scribble/base
@@ -8,16 +7,20 @@
 (provide section*
          racketblock+eval examples0
          make-base-eval
+         full-url-on-github
+         source-code-github-link
          (rename-out [subscript sub]
                      [superscript sup]))
 
+;;; `section` from scribble but do not add a section number before title
 (define (section* . e)
   (section #:style (style #f (list 'unnumbered))
            e))
 
+;;; customized `racketblock` and `examples`
 (define-syntax (racketblock+eval stx)
   (syntax-parse stx
-    [(_ (~or* (~seq #:eval evaluator:expr) (~seq))
+    [(_ (~optional (~seq #:eval evaluator:expr))
         code:expr ...)
      #'(examples (~? (~@ #:eval evaluator))
                  #:no-result
@@ -25,9 +28,20 @@
 
 (define-syntax (examples0 stx)
   (syntax-parse stx
-    [(_ (~or* (~seq #:eval evaluator:expr) (~seq))
+    [(_ (~optional (~seq #:eval evaluator:expr))
         code:expr ...)
      #'(examples (~? (~@ #:eval evaluator))
                  #:label #f
                  code ...)]))
+
+;;; some code examples in the github repo
+(define *code-path-prefix*
+  "https://github.com/yfzhe/yfzhe.github.io/blob/source/")
+
+(define (full-url-on-github path)
+  (string-append *code-path-prefix* path))
+
+(define (source-code-github-link file-path)
+  (hyperlink (string-append *code-path-prefix* "code/" file-path)
+             file-path))
 
